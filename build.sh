e794037af68cfc10be50c941cd2a23d42c9b7fff
naocanmonster <naocanmonster@gmail.com>
2023-05-12 16:49:14 +0800

    fix build target name

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 rm -rf ./build/
 mkdir ./build/
 GOOS=windows GOARCH=amd64 go build -o ./build/blazehttp_windows.exe   ./cmd/blazehttp
-GOOS=darwin  GOARCH=arm64 go build -o ./build/blazehttp_mac_m1.exe    ./cmd/blazehttp 
-GOOS=darwin  GOARCH=amd64 go build -o ./build/blazehttp_mac_x86.exe   ./cmd/blazehttp 
-GOOS=linux   GOARCH=arm64 go build -o ./build/blazehttp_linux_arm.exe ./cmd/blazehttp 
-GOOS=linux   GOARCH=amd64 go build -o ./build/blazehttp_linux_x86.exe ./cmd/blazehttp 
+GOOS=darwin  GOARCH=arm64 go build -o ./build/blazehttp_mac_m1        ./cmd/blazehttp 
+GOOS=darwin  GOARCH=amd64 go build -o ./build/blazehttp_mac_x64       ./cmd/blazehttp 
+GOOS=linux   GOARCH=arm64 go build -o ./build/blazehttp_linux_arm64   ./cmd/blazehttp 
+GOOS=linux   GOARCH=amd64 go build -o ./build/blazehttp_linux_x64     ./cmd/blazehttp 
